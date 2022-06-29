93f4a4f5b5fb07b9b96d64a02a1c15c130ec6f30
Hintz <nico.hintz@hotmail.com>
2022-06-29 13:48:00 -0300

    last commit

--- JS/ciclo.js
+++ JS/ciclo.js
@@ -1,72 +1,91 @@
-const cotizarSeguro = () => {
 
-  let marca = document.querySelector("#marca").value;
-  let anio = document.querySelector("#anio").value;
-  let tipoSeguro = Array.from(document.getElementsByName("tipo")).find(r => r.checked).value;
-  let divResultado = document.querySelector("#resultado");
+const marcasJSON = `
+[
+  {
+    "nombre": "Citroen",
+    "precio": 10000
+  },
+  {
+    "nombre": "Fiat",
+    "precio": 15000
+  }
+]
+`;
 
-  if (marca === "" || anio === "" || tipoSeguro === "") {
-    return alert("Falta ingresar algun dato")
-  }
-  if (tipoSeguro === "basico") {
-    const suma = (a, b, c, d) => a + b + c
-    const multiplicacion = (suma, c) => suma * c
-    const iva = x => x * 0.22
-    let precioProducto = 10000
-    let yearProducto = 0.025 * precioProducto
-    let nuevoPrecio = suma(precioProducto, iva(precioProducto), yearProducto)
-    console.log(nuevoPrecio)
+const marcas = JSON.parse(marcasJSON) || [];
 
-    divResultado.innerHTML = `<h2>El precio de tu seguro es $${nuevoPrecio} </h2>`
-  }
-  if (tipoSeguro === "completo") {
-    const suma = (a, b, c, d) => a + b + c 
-    const multiplicacion = (suma, c) => suma * c
-    const iva = x => x * 0.22
-    let precioProducto = 10000
-    let yearProducto = 0.025 * precioProducto
-    let nuevoPrecio = suma(precioProducto, iva(precioProducto), yearProducto) * 2
-    console.log(nuevoPrecio)
-    divResultado.innerHTML = `<h2>El precio de tu seguro es $${nuevoPrecio} </h2>`
-  }
+const actualizarHistorial = (nuevoHistorial) => {
+if (!nuevoHistorial || nuevoHistorial.length === 0) return;
+
+const historialUI = document.querySelector("#historial");
+
+historialUI.innerHTML = nuevoHistorial.map(cotizacion => (
+    `<li>
+      <div>Marca: ${cotizacion.marca}, Año: ${cotizacion.anio}, Tipo: ${cotizacion.tipoSeguro}, Precio: $${cotizacion.precio}</div>
+    </li>`
+  )
+  );
 };
 
+// Funcion autoejecutable
+(function () {
+const historialStr = localStorage.getItem('historial') || "[]";
+const historial = JSON.parse(historialStr);
 
+actualizarHistorial(historial);
+})();
 
-// const cotizarSeguro = () => {
+const guardarCotizacion = (marca, anio, tipoSeguro, precio) => {
+const historialStr = localStorage.getItem('historial');
+const cotizacion = {
+  marca,
+  anio,
+  tipoSeguro,
+  precio
+}
+let historial = JSON.parse(historialStr);
 
-//   let marca = document.querySelector("#marca").value;
-//   let anio = document.querySelector("#anio").value;
-//   let basico = document.querySelector("#basico");
-//   let completo = document.querySelector("#completo");
-//   let divResultado = document.querySelector("#resultado");
-//   let tipoSeguro = "";
+if (!historialStr) {
+  window.localStorage.setItem('historial', JSON.stringify([cotizacion]));
+} else {
+  historial.push(cotizacion);
+  if (historial.length > 5) historial.shift();
+  localStorage.setItem('historial', JSON.stringify(historial));
+}
 
-//   if (basico.checked) {
-//     plan = "basico"
-//   } else if (completo.checked) {
+actualizarHistorial(historial);
+};
 
-//     plan = "completo"
-//   }
+const calcularPrecio = (marca, anio, tipoSeguro) => {
+// Acá primero busco en el array de marcas si hay alguno con el mismo
+// nombre que seleccionó el usuario y le saco el precio.
+// En caso de no encontrarlo, le pone 20000 de precio 
+let precio = marcas.find(({ nombre }) => nombre.toLowerCase() === marca.toLowerCase())?.precio || 20000;
 
-//   if (marca === "" || anio === "" || tipoSeguro === "") {
-//     alert("Falta ingresar algun dato")
-//     return
-//   }
+if (anio > 2008) precio *= 1.2;
+if (tipoSeguro === 'completo') precio *= 1.5;
 
-// }
+return precio;
+};
 
-// function cotizarSeguro(){
+const cotizarSeguro = () => {
+const marca = document.querySelector('#marca').value;
+const anio = document.querySelector('#anio').value;
+const tipoSeguro = document.querySelector('[name="tipoSeguro"]:checked').value;
+const resultado = document.querySelector('#resultado');
 
+if (!marca || !anio || !tipoSeguro) {
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: 'Debes completar todos los campos!',
+    footer: '<a href="">Why do I have this issue?</a>'
+  });
+  return;
+};
 
+const precio = calcularPrecio(marca, anio, tipoSeguro);
 
-//   alert("El precio de tu seguro es " + nuevoPrecio + " IVA incluido")
-// }
-
-// Creo un array para ir agregando marcas que solicite el usuario al listado //
-
-// let nuevasMarcas = ["Citroen", "Peugeot", "Fiat"]
-
-// nuevasMarcas.push(promt("Que marca desea cotizar"))
-// console.log(nuevasMarcas)
-
+guardarCotizacion(marca, anio, tipoSeguro, precio);
+resultado.innerHTML = `<h2>El precio es: $${precio}</h2>`;
+}
